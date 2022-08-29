48debfd6ca59e850dfc5c5037c5be21a071bad5a
sfxfs <2501683578@qq.com>
2022-08-29 15:42:36 +0800

    v0.9

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 This is a example program*/
 #include <VFD.h>
 
+byte image[5] = {0x00, 0x01, 0x57, 0x88, 0x00};
+
 void setup(){
   SPI_Init();
   VFD_Init();
@@ -9,4 +11,6 @@ void setup(){
 
 void loop(){
   VFD_Show_str(0, "12345");
+  delay(1000);
+  VFD_Show_custdata(0, image);
 }
