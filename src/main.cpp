e4f42f02f944875375d717c59e518a069b6b4f12
sfxfs <2501683578@qq.com>
2022-09-13 20:20:06 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ This is a example program*/
 #include <VFD.h> //使用显示驱动前请先包含该头文件
 
 const byte image[5] = {0x7F, 0x6B, 0x5D, 0x6B, 0x7F}; //自定义图像（由取模软件得到）
-VFD_Display VFD;
+VFD_Display VFD;  //实例化对象
 
 void setup()
 {
@@ -31,19 +31,23 @@ void loop()
   delay(1000);
 
   VFD.VFD_Standby_mode(false); //退出省电模式
+  
   for (size_t i = 255; i > 0; i--)
   {
     VFD.VFD_Set_dimming(i); //字符淡出效果
-    delay(10);
+    delay(3);
   }
+  
   VFD.VFD_Clear(-1);                //清除所有位的显示
   VFD.VFD_Write_custdata(0, image); //写入自定义字符，保存为第0个自定义字符
   VFD.VFD_Show_custdata(1, 0);      //在第二位显示第0个自定义字符
-  delay(300);
+  
+  delay(400);
+  
   for (size_t i = 0; i <= 255; i++)
   {
     VFD.VFD_Set_dimming(i); //字符淡入效果
-    delay(10);
+    delay(3);
   }
 
   delay(1000);
